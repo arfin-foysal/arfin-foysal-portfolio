25480882e3790bcbe4e45e636c984f1c3b7ac21f
Arfin Foysal <arfinfoysalbd@gmail.com>
2022-08-17 23:31:16 +0600

    vercel

---
+++ src/content_option.js
@@ -0,0 +1,190 @@
+const logotext = "ARFIN";
+const meta = {
+    title: "Arfin Foysal",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Foysal Ahmmad Arfin.",
+    animated: {
+        first: "I love coding",
+        second: "I code cool Software",
+        third: "I develop apps",
+    },
+    description: "I’m a Software Developer _ Full stack devloper,currently working in BD",
+    your_img_url: "https://miro.medium.com/max/1400/1*q884i9FEPL34MbvM7R_-OA.jpeg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm a full stack Software Developer. I have a passion for building web applications and solving problems. I'm a self-taught developer and I'm always looking for new challenges to learn and grow.",
+};
+const worktimeline = [
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "YAdfi",
+    //     date: "2020",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [
+    {
+        name: "C Programming",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "PHP",
+        value: 70,
+    },
+    {
+        name: "Node Js",
+        value: 80,
+    },
+    {
+        name: "Express Js",
+        value: 80,
+    },
+    {
+        name: "Laravel",
+        value: 50,
+    },
+    {
+        name: "Djano",
+        value: 50,
+    },
+ 
+    {
+        name: "React Js",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "I build web applications using the latest technologies.",
+    },
+    {
+        title: "Software Development",
+        description: "I build Software applications using the latest technologies.",
+    },
+    {
+        title: "Mobile App Development",
+        description: "I build Mobile applications using the latest technologies.",
+    },
+];
+
+const dataportfolio = [
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/800/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "af.arfinfoysal@gmail.com",
+    YOUR_FONE: "+8801780314627",
+    description: "I’m a Software Developer _ Full stack devloper,currently working in BD",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/arfin-foysal",
+    facebook: "https://facebook.com/arfinfoysalbd",
+    linkedin: "https://linkedin.com/in/arfinfoysal",
+    twitter: "https://twitter.com/arfinfoysalbd",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
